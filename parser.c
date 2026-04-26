54d3f6ac16e0e1a51a3b99b07b16148551411052
Vipulav-me <vipulav.batra2028@gmail.com>
2026-04-26 14:19:11 +0530

    Parser functions

--- parser.c
+++ parser.c
@@ -3,38 +3,44 @@
 #include <string.h>
 #include <ctype.h>
 #include "parser.h"
-
+//This function converts column letters to 0-based indexing
+//EG. A:0 AA:26 
 int col_name_to_index(const char *col_str){
-    if (!col_str||*col_str=='\0'){
+    if (!col_str||*col_str=='\0'){//Valid input else -1
          return -1;
     }
     int index = 0;
+    //Processing each char 
+    //Eg: 'AA' is evaluated to ((1*26) + 1) -1 = 26
+    //(-1 Due to 0 based index) 
     for (int i=0;col_str[i]!='\0';i++){
         char c = col_str[i];
         if (!isupper(c)){
-            return -1;
+            return -1; //Only upper case allowed
         }
         index = index*26+(c-'A'+1);
     }
     return index-1; 
 }
-
-void index_to_col_name(int index, char *buf){
+//This basically does the opposite of the above function 
+void index_to_col_name(int index,char *buf){
     char tmp[MAX_CELL_NAME];
     int len = 0;
     index++;
-    while (index>0){
+    while(index>0){
         int rem = (index-1)%26;
         tmp[len++] = 'A'+rem;
         index = (index-1)/26;
     }
-    for (int i=0; i<len;i++){
+    for(int i=0; i<len;i++){
         buf[i] = tmp[len-1-i];
     }
     buf[len] ='\0';
 }
-
-int parse_cell_name(const char *cell_str, int *row, int *col) {
+//What this function does is it parsing the input cell name like Z3 for example
+// And converts it to row and column indices 
+//For this example: row = 2 , column = 25
+int parse_cell_name(const char *cell_str,int *row,int *col) {
     if (!cell_str||!row||!col){
         return 0;
     }
@@ -62,7 +68,8 @@ int parse_cell_name(const char *cell_str, int *row, int *col) {
     *row = (int)(row_num-1);
     return 1;
 }
-
+//Opposite worjing of the above function
+//Row column indices to cell name
 void cell_name_from_indices(int row, int col, char *buf) {
     char col_str[MAX_CELL_NAME];
     index_to_col_name(col,col_str);
